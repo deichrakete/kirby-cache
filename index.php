ed8255ea2f6fd97cb2d7f021d35e68c61a7c00d7
Stephan Ploehn <stephan@foerdeliebe.com>
2023-12-19 16:53:30 +0100

    feat(kirby): added initial files
    - adds Last-Modified header
    - cache buster for js() and css()

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Cache Plugin
+ *
+ * @version   1.0.0-beta.1
+ * @link      https://github.com/foerdeliebe/kirby-cache
+ * @license   MIT
+ */
+
+use Kirby\Cms\App as Kirby;
+use Kirby\Cms\Page;
+use Kirby\Cms\Url;
+use Kirby\Filesystem\F;
+
+/**
+ * Get the file with modified timestamp
+ *
+ * @param Kirby $kirby
+ * @param string $file
+ * @return string
+ */
+function getFileModfied(Kirby $kirby, string $file): string
+{
+    $relative_url = Url::path($file, false);
+    $file_root = $kirby->root('index') . '/' . $relative_url;
+
+    if (F::exists($file_root)) {
+        return url($relative_url . '?' . F::modified($file_root));
+    }
+
+    return $file;
+}
+
+Kirby::plugin('foerdeliebe/cache', [
+    'components' => [
+        'css' => function (Kirby $kirby, string $url, $options = null): string
+        {
+            if (option('foerdeliebe.cache.buster.css')) {
+                return getFileModfied($kirby, $url);
+            }
+
+            return $url;
+        },
+        'js' => function (Kirby $kirby, string $url, $options = null): string
+        {
+            if (option('foerdeliebe.cache.buster.js')) {
+                return getFileModfied($kirby, $url);
+            }
+
+            return $url;
+        },
+    ],
+    'hooks' => [
+        'route:after' => function ($result): Page
+        {
+            if (option('foerdeliebe.cache.last-modified')) {
+                header('Last-Modified: ' . $result->modified('F d Y H:i:s.'));
+            }
+            return $result;
+        },
+    ],
+    'options' => [
+        'buster' => [
+            'css' => true,
+            'js' => true,
+        ],
+        'last-modified' => true,
+    ],
+]);
